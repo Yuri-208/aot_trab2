ffeb16bc7dddffe19365abb0ffa93696d9e261c3
Yuri-208 <ryuri8161@gmail.com>
2025-08-27 20:07:22 -0300

    Update base_trab_2.ino
    Adição de comentários em Inglês.

--- base_trab_2.ino
+++ base_trab_2.ino
@@ -3,15 +3,15 @@
 //PROF.: Glauber Kiss de Souza
 //DISC.: Analizar Orient. Técnicas
 
-int soma = 1;  // Ativar a operação de soma
+int soma = 1;  // Enable the sum operation
 int carryBit = 0;
-int nib1a,nib1b,nib1c,nib1d = 0; // Peimeiro número binário 
-int nib2a,nib2b,nib2c,nib2d = 0; // Segundo número binário 
-int res1a,res1b,res1c,res1d = 0; // Resultado da soma 
+int nib1a,nib1b,nib1c,nib1d = 0; // First binary number
+int nib2a,nib2b,nib2c,nib2d = 0; // Second binary number
+int res1a,res1b,res1c,res1d = 0; // Sum result
 
 void setup()
 {
-	// Pinos de Entrada
+	// Input pins
 	pinMode(0, INPUT);
 	pinMode(1, INPUT);
 	pinMode(2, INPUT);
@@ -21,18 +21,18 @@ void setup()
 	pinMode(6, INPUT);
 	pinMode(7, INPUT);
 
-	// Configura os pinos de saída 
+	// Configures the output pins 
 	pinMode(8, OUTPUT);
 	pinMode(9, OUTPUT);
 	pinMode(10, OUTPUT);
 	pinMode(11, OUTPUT);
 	pinMode(12, OUTPUT);
 
-	// Pode ser usado como botão de controle
+	// Can be used as a control button
 	pinMode(13, INPUT);
 }
 
-// Soma dois bits com carry e transforma e retorna o bit de resultado
+// Adds two bits with carry and transform and returns the result bit
 int somaBit(int b1a, int b2a, int cBit)
 {
 	int bitResult = 0;
@@ -47,7 +47,7 @@ int somaBit(int b1a, int b2a, int cBit)
 	}
 	return bitResult;
 }
-// Função que calcula o novo carry após a soma de dois bits 
+// Function that calculates the new carry after the addition of two bits 
 int somaCarryBit(int b1a, int b2a, int cBit)
 {
 	int aux1, aux2 = 0;
@@ -64,13 +64,13 @@ int somaCarryBit(int b1a, int b2a, int cBit)
 
 void loop()
 {
-	soma = 1; // Ativa a soma 
-    // leitura dos bits do primeiro número
+	soma = 1; // Activate the sum 
+    // reading the bits of the first number
 	nib1a = digitalRead(0);
 	nib1b = digitalRead(1);
 	nib1c = digitalRead(2);
 	nib1d = digitalRead(3);
-	// Leituras dos bits do segundo número
+	// Reading of the bits of the second number
 	nib2a = digitalRead(4);
 	nib2b = digitalRead(5);
 	nib2c = digitalRead(6);
@@ -78,7 +78,7 @@ void loop()
 	if (soma == 1)
 	{
 		carryBit = 0;
-		// Soma bit a bit com a programção carry
+		// Bitwise addition with carry programming
 		res1a = somaBit(nib1a,nib2a,carryBit);
 		carryBit = somaCarryBit(nib1a,nib2a,carryBit);
 		res1b = somaBit(nib1b,nib2b,carryBit);
@@ -88,11 +88,12 @@ void loop()
 		res1d = somaBit(nib1d,nib2d,carryBit);
 		carryBit = somaCarryBit(nib1d,nib2d,carryBit);
 	}
-	// Exibe os bits do resultado nos pinos de saída 
+	// Displays the result bits on the output pins 
 	digitalWrite(8,res1a);
 	digitalWrite(9,res1b);
 	digitalWrite(10,res1c);
 	digitalWrite(11,res1d);
-	digitalWrite(12,carryBit); // Exibe o bit de transporte final
+	digitalWrite(12,carryBit); // Displays the final carry bit
 }
+
 
